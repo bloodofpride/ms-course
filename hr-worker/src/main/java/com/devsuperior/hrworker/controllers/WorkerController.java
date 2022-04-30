c31305d09d1934076269e1ce3e181e36d7f47f52
Maxwell_Ponte <antonio_maxwell@hotmail.com>
2022-04-30 19:08:31 -0300

    Ribbo load balancing

--- hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
@@ -2,6 +2,10 @@ package com.devsuperior.hrworker.controllers;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.services.WorkerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +17,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
     private WorkerService workerService;
+    private Environment env;
+    
 
-    public WorkerController(WorkerService workerService){
+    public WorkerController(WorkerService workerService,  Environment env){
         this.workerService = workerService;
+        this.env = env;
     }
 
     @GetMapping()
@@ -26,6 +34,7 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         return ResponseEntity.ok().body(workerService.findById(id));
     }
 }
