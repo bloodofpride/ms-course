9ca90652eb069bd99c2edaa1c32ee2dfb80c9fea
Maxwell_Ponte <antonio_maxwell@hotmail.com>
2022-04-30 19:09:12 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/controllers/PaymentController.java
@@ -5,8 +5,6 @@ import com.devsuperior.hrpayroll.services.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
